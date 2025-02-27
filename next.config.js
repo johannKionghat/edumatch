46baa20a23bc0ebbfbbcd2f2bd77673c378f55db
DESKTOP-ACDD6TV\User <guewoljohannkionghat@gmail.com>
2025-02-27 10:31:06 +0100

    fix: update configuration for Netlify deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
